4d1d4398a6b9795e26e1430f077629839367845d
CheSubhro <mitsubhra@gmail.com>
2024-05-22 15:51:34 +0530

    Some changes in backend and frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import Home from '../src/components/pages/Home'
-// import Search from './components/pages/Search';
 
 function App() {
 	return (
 		<>
 			<Home/>
-			{/* <Search/> */}
 		</>
 	);
 }
